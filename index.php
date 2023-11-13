ec855010bcffa0dfc787bc58fba5cca5a6a446c7
Ricardo Iorio <archivoiorio@proton.me>
2023-11-13 05:20:39 -0300

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dir = ".";
-$files = scandir($dir);
-foreach($files as $file) {
-    if ($file != '.' && $file != '..') {
-        echo '<a href="'.$file.'">'.$file.'</a><br>';
-    }
-}
-?>
